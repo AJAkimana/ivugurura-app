a8151db9ac078fc81cd392d36c86ebfb2b5ba045
Akimana J d Amour <akimanaja17@gmail.com>
2021-07-29 16:57:25 +0200

    Create the base reducer

---
+++ lib/core/redux/base_reducer.dart
@@ -0,0 +1,13 @@
+import 'package:ivugurura_app/core/redux/base_action.dart';
+import 'package:ivugurura_app/core/redux/base_state.dart';
+
+baseReducer(BaseState<dynamic> prevState, BaseAction<dynamic> action){
+  final payload = action.state;
+  return prevState.copyWith(
+    loading: payload.loading,
+    loaded: payload.loaded,
+    error: payload.error,
+    theList: payload.theList,
+    theObject: payload.theObject
+  );
+}
