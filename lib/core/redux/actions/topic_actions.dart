c2c47b94a5d1c6c67d0a727d8c8b27c930e73291
Akimana J d Amour <akimanaja17@gmail.com>
2021-07-29 17:00:48 +0200

    Apply new state and action behavior to the topic

--- lib/core/redux/actions/topic_actions.dart
+++ lib/core/redux/actions/topic_actions.dart
@@ -1,22 +1,16 @@
 import 'dart:convert';
 
-import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 import 'package:ivugurura_app/core/models.dart';
-import 'package:ivugurura_app/core/redux/states/topics_states.dart';
 import 'package:ivugurura_app/core/redux/store.dart';
 import 'package:ivugurura_app/core/utils/constants.dart';
-import 'package:redux/redux.dart';
 
-@immutable
-class TopicsActions{
-  final TopicsState topicsState;
+import '../base_action.dart';
 
-  TopicsActions(this.topicsState);
-}
 
 Future<void> fetchTopics({int page=1, int pageSize=4, String category='carsoul'}) async {
-  appStore.dispatch(TopicsActions(TopicsState(loading: true)));
+  DispatchedAction<Topic> dispatchedAction = DispatchedAction();
+  appStore.dispatch(dispatchedAction.pending());
   try{
     String url = '$topicsUrl?page=$page&pageSize=$pageSize&category=$category';
     final res = await http.get(
@@ -25,14 +19,15 @@ Future<void> fetchTopics({int page=1, int pageSize=4, String category='carsoul'}
         );
     assert(res.statusCode < 400);
     final jsonData = json.decode(res.body)['data'] as List;
-    // print(jsonData);
     List<Topic> topicsData = jsonData.map((e) => Topic.fromJson(e)).toList();
-    appStore.dispatch(TopicsActions(TopicsState(loading: false, loaded: true, topics: topicsData)));
+    dispatchedAction.setActionValue(topicsData);
+    appStore.dispatch(dispatchedAction.fulfilled());
   } catch (error){
     print('{=======================');
     print(error);
     print(topicsUrl);
     print('=========================}');
-    appStore.dispatch(TopicsActions(TopicsState(loading: false, error: error.toString())));
+    dispatchedAction.setActionValue(error.toString());
+    appStore.dispatch(dispatchedAction.rejected());
   }
 }
