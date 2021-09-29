39365a8d94d96c801a3e9b2f3a64b197d6d07a79
Akimana A Jean d Amour <akimanaja17@gmail.com>
2021-09-29 12:11:20 +0200

    Finish paginated scroll view

--- lib/pages/topics_page.dart
+++ lib/pages/topics_page.dart
@@ -73,65 +73,64 @@ class TopicsPageState extends State<TopicsPage> {
                   )));
         });
   }
-  Widget _buildListTopics(dynamic category){
+
+  Widget _buildListTopics(dynamic category) {
     return StoreConnector<AppState, BaseState<Topic, CategoryTopic>>(
       distinct: true,
-      onInitialBuild: (store){
+      onInitialBuild: (store) {
         fetchTopics(context, category: category);
       },
       converter: (store) => store.state.categoryTopic,
-      builder: (context, categoryTopicsState){
+      builder: (context, categoryTopicsState) {
         List<Topic> categoryTopics = categoryTopicsState.theList!;
         // print(categoryTopics);
         // print(category);
         // print(currentIndex);
-        if(categoryTopicsState.loading){
+        if (categoryTopicsState.loading) {
           return DotsLoader();
         }
-        if(categoryTopicsState.error!=''){
+        if (categoryTopicsState.error != '') {
           return DisplayError(error: categoryTopicsState.error);
         }
-        if(categoryTopicsState.theList!.length < 1){
+        if (categoryTopicsState.theList!.length < 1) {
           return NoDisplayData();
         }
-        return  ListView.builder(
+        return ListView.builder(
           shrinkWrap: true,
           physics: NeverScrollableScrollPhysics(),
           itemCount: categoryTopics.length,
-          itemBuilder: (BuildContext context, int index){
+          itemBuilder: (BuildContext context, int index) {
             return RecentTopicItem(topic: categoryTopics[index]);
           },
         );
       },
     );
   }
+
   List<Widget> buildCategoriesTabs(List<Category> categories, int currIndex) {
     Widget firstCategory = Center(
       child: Text('Recent topics',
           textAlign: TextAlign.center,
-          style: Theme.of(context).textTheme.headline4
-      ),
+          style: Theme.of(context).textTheme.headline4),
     );
-    if(currIndex==0){
+    if (currIndex == 0) {
       firstCategory = _buildListTopics('recent');
     }
-    final theList = <Widget>[
-      firstCategory
-    ];
+    final theList = <Widget>[firstCategory];
     for (var category in categories) {
       Widget currCategory = Center(
-        child: Text(category.name??'',
+        child: Text(category.name ?? '',
             textAlign: TextAlign.center,
-            style: Theme.of(context).textTheme.headline4
-        ),
+            style: Theme.of(context).textTheme.headline4),
       );
-      if(currIndex!=0){
+      if (currIndex != 0) {
         currCategory = _buildListTopics(category.id);
       }
       theList.add(currCategory);
     }
     return theList;
   }
+
   List<Widget> buildCategoriesHeader(List<Category> categories) {
     final theList = <Widget>[
       Padding(
--- lib/widget/topic_list_item.dart
+++ lib/widget/topic_list_item.dart
@@ -3,15 +3,14 @@ import 'package:flutter_html/flutter_html.dart';
 import 'package:intl/intl.dart';
 import 'package:ivugurura_app/core/models/topic.dart';
 import 'package:ivugurura_app/core/utils/constants.dart';
+import 'package:ivugurura_app/pages/one_topic_view.dart';
 import 'package:ivugurura_app/widget/network_image.dart';
 import 'package:truncate/truncate.dart';
+import 'package:ivugurura_app/core/extensions/string_cap_extension.dart';
 
 class TopicListItem extends StatelessWidget {
   final Topic topic;
-  const TopicListItem({
-    Key? key,
-    required this.topic
-  }) : super(key: key);
+  const TopicListItem({Key? key, required this.topic}) : super(key: key);
 
   DateTime get dateTime => DateTime.parse(topic.createdAt as String);
 
@@ -20,13 +19,17 @@ class TopicListItem extends StatelessWidget {
     final textTheme = Theme.of(context).textTheme;
 
     return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(6)
-      ),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(6)),
       child: InkWell(
-        onTap: (){},
+        onTap: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => OneTopicViewPage(topicSlug: topic.slug),
+              ));
+        },
         child: Padding(
-          padding: const EdgeInsets.all(16),
+          padding: const EdgeInsets.all(8),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
@@ -34,21 +37,29 @@ class TopicListItem extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
                   Expanded(
-                    child: Text(topic.title, style: textTheme.headline6),
+                    child: Text(topic.title.toLowerCase().capitalizeFirstOfEach, style: textTheme.headline6),
                   ),
-                  const SizedBox(width: 16),
+                  const SizedBox(width: 8),
                   ClipRRect(
                     borderRadius: BorderRadius.circular(8),
-                    child: PNetworkImage("$IMAGE_PATH/${topic.coverImage}", height: 50, width: 50),
+                    child:Container(
+                      height: 50,
+                      width: 50,
+                      margin: const EdgeInsets.only(right: 5.0),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.blue.shade300
+                      ),
+                      child: PNetworkImage(
+                        "$IMAGE_PATH/${topic.coverImage}",
+                        fit: BoxFit.cover,
+                      ),
+                    )
                   )
                 ],
               ),
-              const SizedBox(height: 16),
+              const SizedBox(height: 8),
               Html(data: truncate(topic.content as String, 150, omission: '...')),
-              // Text(
-              //  '',
-              //   style: textTheme.bodyText2
-              // ),
               Text(DateFormat('MMM d yyyy').format(dateTime))
             ],
           ),
