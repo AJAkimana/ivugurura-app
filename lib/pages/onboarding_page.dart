0af07489336769bc1002d94dea5bc88e1967b37b
Jean dAmour AKIMANA(MBP) <akimanaja17@gmail.com>
2025-01-08 09:06:16 +0200

    Cleanup the code

--- lib/pages/onboarding_page.dart
+++ lib/pages/onboarding_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_swiper_null_safety/flutter_swiper_null_safety.dart';
 import 'package:flutter_translate/flutter_translate.dart';
